1daf5b24e7c1a1a84096565fa5e0de73aaf821d4
Andrei Fokau <andrei@5monkeys.se>
2014-07-24 12:15:24 +0200

    Fix broker-dealer

--- blue/client.py
+++ blue/client.py
@@ -14,23 +14,18 @@ from utils import get_local_ip
 
 context = zmq.Context()
 
-dealer_socket = context.socket(zmq.DEALER)
-router_socket = context.socket(zmq.ROUTER)
+socket = context.socket(zmq.DEALER)
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
-#
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--connect-address', default='tcp://127.0.0.1:5555')
 
-dealer_socket.connect('tcp://127.0.0.1:5555')
-router_socket.connect('tcp://127.0.0.1:5556')
+args = parser.parse_args()
+
+socket.connect(args.connect_address)
+
 while 1:
     msg = "{}:{}".format(get_local_ip(), '5555')
     print 'Sending address to the dealer', msg
-    dealer_socket.send(msg)
-    print dealer_socket.recv()
-
-    print 'Asking router...'
-    router_socket.send('WHO')
-    print router_socket.recv()
+    socket.send(msg)
+    print socket.recv()
     time.sleep(1)
